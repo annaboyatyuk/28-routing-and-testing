11927bb8ad98711589aa2840cfd0859cc1c8b0c4
annaboyatyuk <annaboyatyuk@gmail.com>
2018-08-06 23:40:54 -0700

    readme

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -9,6 +9,7 @@ export default class Landing extends React.Component {
         <h2>Welcome to the Notes</h2>
 
         <p>Add notes to this app! delete them if you get sick of them! its fun!</p>
+        <p>click the dashboard to get startedddd</p>
 
       </React.Fragment>
     );
